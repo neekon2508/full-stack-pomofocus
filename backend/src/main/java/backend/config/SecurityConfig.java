be485683b117b941885e6f6c7bda07739cfe0015
Neekon <nghia250803@gmail.com>
2026-01-09 17:20:49 +0700

    Backend: Demo for OAuth2 Specification.

--- backend/src/main/java/backend/config/SecurityConfig.java
+++ backend/src/main/java/backend/config/SecurityConfig.java
@@ -1,9 +1,81 @@
 package backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    @Value("${google.client-id}")
+    private String clientId;
+
+    @Value("${google.client-secret}")
+    private String clientSecret;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .cors(Customizer.withDefaults())
+            .csrf(csrf -> csrf.disable())
+            .oauth2Login(o -> o.defaultSuccessUrl("http://localhost:5173/login", true))
+            .authorizeHttpRequests(
+                c -> c.anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(o -> o.jwt(Customizer.withDefaults()))
+        ;
+        
+
+        return http.build();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+
+        ClientRegistration c1 = ClientRegistration.withRegistrationId("1")
+            .clientId(clientId)
+            .clientSecret(clientSecret)
+            .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
+            .tokenUri("https://oauth2.googleapis.com/token")
+            .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+            .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
+            .redirectUri("http://localhost:8080/login/oauth2/code/google")
+
+            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+            .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+            .tokenUri("https://oauth2.googleapis.com/token")
+            .scope(OidcScopes.OPENID)
+            .userNameAttributeName("sub")
+            .build();
+        
+        return new InMemoryClientRegistrationRepository(c1);
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager(
+        ClientRegistrationRepository clientRegistrationRepository, OAuth2AuthorizedClientRepository auth2AuthorizedClientRepository) {
+        var provider = OAuth2AuthorizedClientProviderBuilder.builder().authorizationCode().refreshToken().build();
+
+        var cm = new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, auth2AuthorizedClientRepository);
+
+        cm.setAuthorizedClientProvider(provider);
+
+        return cm;
+
+    }
     
 }
